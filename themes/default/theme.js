87375601642088f1f6a0d01cea4ece7cdbf3dd43
frendly <www.zpdf@mail.ru>
2018-10-24 14:20:36 +0300

    init

---
+++ themes/default/theme.js
@@ -0,0 +1,12 @@
+// console.log("Default theme is loaded")
+import tippy from './js/tippy.all.min.js';
+import './style.styl';
+
+tippy.setDefaults({
+  arrow: true,
+  placement: "left",
+  // arrowType: 'round',
+  // duration: 0,
+  trigger: "click",
+  // theme: "light-border"
+})
